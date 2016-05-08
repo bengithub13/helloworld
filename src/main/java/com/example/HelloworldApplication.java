b5e71b43ef19cff5ae156f438d41bf4004ae4d8d
BenPoon <BenPoon@Bens-asus-pc.home>
2016-05-08 00:41:08 -0400

    test config @value

---
+++ src/main/java/com/example/HelloworldApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/HelloworldApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HelloworldApplication.class)
+@ActiveProfiles("junit")
+public class HelloworldApplicationTests {
+	@Value("${testing.test}")
+	private String testing;
+	@Test
+	public void contextLoads() {
+		System.out.println(testing);
+	}
+
+}
